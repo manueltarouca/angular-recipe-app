862f180996637d0dc6e45e92c197dbcd90a908d8
Manuel Martins <manuemartins@deloitte.pt>
2021-05-30 18:35:29 +0100

    Components & Databinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-project';
+  
+  loadedFeature: string = 'recipes';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,15 +8,21 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe("A test recipe", "Test description...", "https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg"),
-    new Recipe("A test recipe", "Test description...", "https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg"),
-    new Recipe("A test recipe", "Test description...", "https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg")
+    new Recipe("Another test recipe", "Test descriptiossssssssssn...", "https://images.food52.com/S_S9Xq9IsgoLENFJkfKvkvwSChg=/1200x1200/a4ad34a1-b43b-42d6-8a73-b3d5f19d11f3--2020-0218_asparagus-lemon-pepper-marinade-genius_3x2_ty-mecham.jpg"),
+    new Recipe("Yet another test recipe", "Test descrip13eetion...", "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/garlic-spaghetti-horizontal-1539203011.jpg?crop=0.670xw:1.00xh;0.167xw,0&resize=640:*")
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe)
   }
 
 }
